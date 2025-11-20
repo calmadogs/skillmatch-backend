65f183fa51c0bb3c9fb2dcec14c78bf5aaecf2e8
calmadogs <carlosjrdev23@gmail.com>
2025-11-19 21:05:04 -0300

    criado end-point getMe para usar no front

--- src/controllers/getMeController.ts
+++ src/controllers/getMeController.ts
@@ -1,20 +1,20 @@
-import { Request, Response } from "express";
-import prisma from "../../prisma/client";
+  import { Request, Response } from "express";
+  import prisma from "../../prisma/client";
 
-export const getMe = async (req: Request, res: Response) => {
-  try {
-    const userId = (req as any).user?.userId;
+  export const getMe = async (req: Request, res: Response) => {
+    try {
+      const userId = (req as any).user?.userId;
 
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      select: { id: true, name: true, email: true, role: true }
-    });
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { id: true, name: true, email: true, role: true }
+      });
 
-    if (!user) return res.status(404).json({ error: "Usuário não encontrado" });
+      if (!user) return res.status(404).json({ error: "Usuário não encontrado" });
 
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao carregar dados do usuário" });
-  }
-};
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erro ao carregar dados do usuário" });
+    }
+  };
